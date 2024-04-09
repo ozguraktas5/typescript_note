6e8d03e64e873d2bb6daae14ae3e5e516aaf6029
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-04-09 22:12:18 +0300

    work

---
+++ handbook.js
@@ -0,0 +1,9 @@
+// const user = {
+//     name: "Daniel",
+//     age: 34
+// };
+// user.location;
+var announcement = "Hello World!";
+announcement.toLocaleLowercase();
+announcement.toLocalLowerCase();
+announcement.toLocaleLowerCase();
---
+++ handbook.ts
@@ -0,0 +1,19 @@
+// const user = {
+//     name: "Daniel",
+//     age: 34
+// };
+
+// user.location;
+
+// const announcement = "Hello World!";
+
+// announcement.toLocaleLowercase();
+// announcement.toLocalLowerCase();
+// announcement.toLocaleLowerCase();
+
+// function flipCoin() {
+//     return Math.random < 0.5;
+// }
+
+
+
