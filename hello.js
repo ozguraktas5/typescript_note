350266637b264908e98cf2e09e0b4da775336aa0
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-04-10 13:49:24 +0300

    work

--- hello.js
+++ hello.js
@@ -2,7 +2,41 @@
 //   console.log(`Hello ${person}, today is ${date}, age is ${age}!`);
 // }
 // greet("Brendan", 2024);
-function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+// greet("Maddison", new Date());
+// let obj: any = { x:0 };
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const n: number = obj;
+// function greet(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+// };
+// greet(32)
+// function getFavouriteNumber(): number {
+//     return 22
+// }
+// async function getFavouriteNumber(): Promise<number> {
+//     return 16;
+// };
+// const names = ["Alice", "Bob", "Eve"];
+// names.forEach(function (s) {
+//     console.log(s.toUpperCase());
+// });
+// names.forEach((s) => {
+//     console.log(s.toUpperCase());
+// });
+// function printCoord( pt: {x: number, y: number}) {
+//     console.log("The coordinate's x: " + pt.x)
+//     console.log("The coordinate's y: " + pt.y)
+// }
+// printCoord({ x:  5, y: 7 });
+function printName(obj) {
+    return "Hello ".concat(obj.firstName, " ").concat(obj.lastName);
 }
-greet("Maddison", new Date());
+;
+console.log(printName({ firstName: "John" }));
+console.log(printName({ firstName: "Michael", lastName: "Jordan" }));
--- hello.ts
+++ hello.ts
@@ -4,8 +4,54 @@
  
 // greet("Brendan", 2024);
 
-function greet(person: string, date: Date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
 
-greet("Maddison", new Date());
+// greet("Maddison", new Date());
+
+// let obj: any = { x:0 };
+
+// obj.foo();
+// obj();
+// obj.bar = 100;
+// obj = "hello";
+// const n: number = obj;
+
+// function greet(name: string) {
+//     console.log("Hello, " + name.toUpperCase() + "!!");
+// };
+
+// greet(32)
+
+// function getFavouriteNumber(): number {
+//     return 22
+// }
+
+// async function getFavouriteNumber(): Promise<number> {
+//     return 16;
+// };
+
+// const names = ["Alice", "Bob", "Eve"];
+
+// names.forEach(function (s) {
+//     console.log(s.toUpperCase());
+// });
+
+// names.forEach((s) => {
+//     console.log(s.toUpperCase());
+// });
+
+// function printCoord( pt: {x: number, y: number}) {
+//     console.log("The coordinate's x: " + pt.x)
+//     console.log("The coordinate's y: " + pt.y)
+// }
+
+// printCoord({ x:  5, y: 7 });
+
+function printName(obj: { firstName: string, lastName?: string }) {
+    return `Hello ${obj.firstName} ${obj.lastName}`;
+};
+
+console.log(printName( { firstName : "John" } ));
+console.log(printName( { firstName: "Michael", lastName: "Jordan" } ));
