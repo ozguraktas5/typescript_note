7edd11868cd4388565828beeeaca8ea55896c693
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-04-11 19:14:10 +0300

    work

--- moreonfunctions.ts
+++ moreonfunctions.ts
@@ -43,14 +43,76 @@
 // const u = firstElement([]);
 // console.log(u)
 
-function map<Input, Output>(
-  arr: Input[],
-  func: (arg: Input) => Output
-): Output[] {
-  return arr.map(func);
+// function map<Input, Output>(
+//   arr: Input[],
+//   func: (arg: Input) => Output
+// ): Output[] {
+//   return arr.map(func);
+// }
+
+// // Parameter 'n' is of type 'string'
+// // 'parsed' is of type 'number[]'
+// const parsed = map(["1", "2", "3"], (n) => parseInt(n));
+// console.log(parsed)
+
+// function longest<Type extends { length: number }>(a: Type, b: Type) {
+//     if(a.length >= b.length) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+// const longerArray = longest([1,2], [1,2,3])
+// console.log(longerArray)
+
+// const longerString = longest("ozgur", "ebru")
+// console.log(longerString)
+
+// const notOK = longest(10, 1000);
+// console.log(notOK)
+
+// function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+//     return arr1.concat(arr2);
+// }
+
+// const arr1 = combine([1,2,3], ["hello"]);
+// console.log(arr1);
+
+// const arr2 = combine<string | number>([1,2,3], ["hello"]);
+// console.log(arr2);
+
+// function firstElement1<Type>(arr: Type[]) {
+//     return arr[0];
+// }
+
+// function firstElement2<Type extends any[]>(arr: Type) {
+//     return arr[0];
+// }
+
+// const a = firstElement1([1, 2, 3]);
+// console.log(a);
+// const b = firstElement2([1, 2, 3]);
+// console.log(b);
+
+// function greet<Str extends string>(s: Str) {
+//     console.log("Hello " + s);
+// }
+
+// greet("World")
+
+// function greet(s: string) {
+//     console.log("Hello " + s);
+// }
+
+// greet("World")
+
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i);
+    }
 }
 
-// Parameter 'n' is of type 'string'
-// 'parsed' is of type 'number[]'
-const parsed = map(["1", "2", "3"], (n) => parseInt(n));
-console.log(parsed)
+myForEach([1, 2, 3], (a) => console.log(a));
+myForEach([1, 2, 3], (a, i) => console.log(a, i));
+
