152fc15aaa801233c2de8c69d9093dc60ce97b38
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-04-11 12:51:01 +0300

    work

--- narrowing.js
+++ narrowing.js
@@ -28,17 +28,67 @@
 //   }
 //   return "Nobody's here. :(";
 // }
-function printAll(strs) {
-    if (strs && typeof strs === "object") {
-        for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
-            var s = strs_1[_i];
-            console.log(s);
-        }
-    }
-    else if (typeof strs === "string") {
-        console.log(strs);
-    }
-}
-printAll(null);
-printAll(["foo", "bar"]);
-printAll("1");
+// function printAll(strs: string | string[] | null) {
+//   if (strs && typeof strs === "object") {
+//     for (const s of strs) {
+//       console.log(s);
+//     }
+//   } else if (typeof strs === "string") {
+//     console.log(strs);
+//   }
+// }
+// printAll(null);
+// printAll(["foo", "bar"]);
+// printAll("1")
+// function multiplyAll(
+//   values: number[] | undefined,
+//   factor: number
+// ): number[] | undefined {
+//   if (!values) {
+//     return values;
+//   } else {
+//     return values.map((x) => x * factor);
+//   }
+// }
+// console.log(multiplyAll(undefined, 5));
+// console.log(multiplyAll([2, 3, 4], 5));
+// function example(x: string | number, y: string | boolean) {
+//   if (x === y) {
+//     // We can now call any 'string' method on 'x' or 'y'.
+//     x.toUpperCase();
+//     y.toLowerCase();
+//   } else {
+//     console.log(x);
+//     console.log(y);
+//   }
+// }
+// example("hello", true);
+// example("hello", "hello")
+// interface Container {
+//   value: number | null | undefined;
+// }
+// function multiplyValue(container: Container, factor: number) {
+//   // Remove both 'null' and 'undefined' from the type.
+//   if (container.value != null) {
+//     console.log(container.value);
+//     // Now we can safely multiply 'container.value'.
+//     container.value *= factor;
+//   }
+// }
+// multiplyValue({ value: 10 }, 5);
+// type Fish = { swim: () => void };
+// type Bird = { fly: () => void };
+// type Human = { swim?: () => void; fly?: () => void };
+// function move(animal: Fish | Bird | Human) {
+//   if ("swim" in animal) {
+//     animal;
+//   } else {
+//     animal;
+//   }
+//   return animal.fly();
+// }
+var x = Math.random() < 0.5 ? 10 : "hello world";
+x = 1;
+console.log(x);
+x = "goodbye";
+console.log(x);
--- narrowing.ts
+++ narrowing.ts
@@ -15,8 +15,6 @@
 
 // console.log(padLeft(5, "Hello"));
 
-
-
 // function printAll(strs: string | string[] | null) {
 //   if (typeof strs === "object") {
 //     for (const s of strs) {
@@ -36,16 +34,106 @@
 //   return "Nobody's here. :(";
 // }
 
-function printAll(strs: string | string[] | null) {
-  if (strs && typeof strs === "object") {
-    for (const s of strs) {
-      console.log(s);
+// function printAll(strs: string | string[] | null) {
+//   if (strs && typeof strs === "object") {
+//     for (const s of strs) {
+//       console.log(s);
+//     }
+//   } else if (typeof strs === "string") {
+//     console.log(strs);
+//   }
+// }
+
+// printAll(null);
+// printAll(["foo", "bar"]);
+// printAll("1")
+
+// function multiplyAll(
+//   values: number[] | undefined,
+//   factor: number
+// ): number[] | undefined {
+//   if (!values) {
+//     return values;
+//   } else {
+//     return values.map((x) => x * factor);
+//   }
+// }
+
+// console.log(multiplyAll(undefined, 5));
+// console.log(multiplyAll([2, 3, 4], 5));
+
+// function example(x: string | number, y: string | boolean) {
+//   if (x === y) {
+//     // We can now call any 'string' method on 'x' or 'y'.
+//     x.toUpperCase();
+
+//     y.toLowerCase();
+//   } else {
+//     console.log(x);
+
+//     console.log(y);
+//   }
+// }
+
+// example("hello", true);
+// example("hello", "hello")
+
+// interface Container {
+//   value: number | null | undefined;
+// }
+
+// function multiplyValue(container: Container, factor: number) {
+//   // Remove both 'null' and 'undefined' from the type.
+//   if (container.value != null) {
+//     console.log(container.value);
+
+//     // Now we can safely multiply 'container.value'.
+//     container.value *= factor;
+//   }
+// }
+
+// multiplyValue({ value: 10 }, 5);
+
+// type Fish = { swim: () => void };
+// type Bird = { fly: () => void };
+// type Human = { swim?: () => void; fly?: () => void };
+
+// function move(animal: Fish | Bird | Human) {
+//   if ("swim" in animal) {
+//     animal;
+//   } else {
+//     animal;
+//   }
+
+//   return animal.fly();
+// }
+
+// let x = Math.random() < 0.5 ? 10 : "hello world";
+
+// x = 1;
+
+// console.log(x);
+
+// x = "goodbye";
+
+// console.log(x);
+
+function example() {
+    let x: string | number | boolean;
+
+    x = Math.random() < 0.5
+
+    console.log(x) // boolean
+
+    if (Math.random() < 0.5) {
+        x = "hello";
+        console.log(x); // string
+    } else {
+        x = 100;
+        console.log(x)  // number
     }
-  } else if (typeof strs === "string") {
-    console.log(strs);
-  }
+
+    return x; 
 }
 
-printAll(null);
-printAll(["foo", "bar"]);
-printAll("1")
+
